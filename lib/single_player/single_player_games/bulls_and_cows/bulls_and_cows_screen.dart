fed2e3986ec1921ac501966d63a002b48cb979a3
ART-SALI <rekudaasnin@gmail.com>
2024-03-26 19:31:11 +0200

    ADDED DISPOSE WITH TEXT_CONTROLLER

--- lib/single_player/single_player_games/bulls_and_cows/bulls_and_cows_screen.dart
+++ lib/single_player/single_player_games/bulls_and_cows/bulls_and_cows_screen.dart
@@ -1,10 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:project_4/single_player/single_player_games/bulls_and_cows/bulls_and_cows.dart';
 
 class BullsAndCowsScreen extends StatefulWidget {
-
-
   const BullsAndCowsScreen({super.key});
 
   @override
@@ -17,18 +14,11 @@ class _BullsAndCowsScreenState extends State<BullsAndCowsScreen> {
   String? textInfo;
   String buttonText = 'Start game';
 
-  TextStyle globalTextStyle = const TextStyle(
-      fontSize: 20.0,
-      color: Colors.white
-  );
-
-
-
-
+  TextStyle globalTextStyle =
+      const TextStyle(fontSize: 20.0, color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: _appBar(),
       backgroundColor: Colors.blueGrey,
@@ -36,68 +26,66 @@ class _BullsAndCowsScreenState extends State<BullsAndCowsScreen> {
     );
   }
 
-
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
 
   AppBar _appBar() {
     return AppBar(
-      title: const Text('Bulls And Cows',
-        style: TextStyle(
-            color: Colors.white
-        ),
+      title: const Text(
+        'Bulls And Cows',
+        style: TextStyle(color: Colors.white),
       ),
       centerTitle: true,
       backgroundColor: Colors.black38,
     );
   }
 
-  Widget _mainBody(){
+  Widget _mainBody() {
     textInfo ??= game.infoText();
     return ListView(
-        children: [
-          Container(
+      children: [
+        Container(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-
-                  const SizedBox(height: 60,),
-
-                  Text(
-                    textInfo!,
-                    textAlign: TextAlign.center,
-                    style: globalTextStyle,
-                  ),
-
-                  const SizedBox(height: 45,),
-
-                  textFormField(),
-
-                  const SizedBox(height: 30,),
-
-                  gameButton(),
-
-                  const SizedBox(height: 30,),
-
-                  surrenderButton(),
-
-                  const SizedBox(height: 200,),
-
-                ],
-          )
-
-        ),],
-
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(
+              height: 60,
+            ),
+            Text(
+              textInfo!,
+              textAlign: TextAlign.center,
+              style: globalTextStyle,
+            ),
+            const SizedBox(
+              height: 45,
+            ),
+            textFormField(),
+            const SizedBox(
+              height: 30,
+            ),
+            gameButton(),
+            const SizedBox(
+              height: 30,
+            ),
+            surrenderButton(),
+            const SizedBox(
+              height: 200,
+            ),
+          ],
+        )),
+      ],
     );
-
   }
 
   TextFormField textFormField() {
     return TextFormField(
       textAlign: TextAlign.center,
       style: globalTextStyle,
-
       controller: myController,
-
       decoration: const InputDecoration(
         filled: true,
         fillColor: Colors.black38,
@@ -105,7 +93,6 @@ class _BullsAndCowsScreenState extends State<BullsAndCowsScreen> {
           borderRadius: BorderRadius.all(Radius.circular(100.0)),
           borderSide: BorderSide(color: Colors.white),
         ),
-
       ),
     );
   }
@@ -114,43 +101,39 @@ class _BullsAndCowsScreenState extends State<BullsAndCowsScreen> {
     return ElevatedButton(
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                if (states.contains(MaterialState.disabled)) {
-                  return Colors.grey;
-                }
-                return Colors.black38;
-              }),
+              (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return Colors.grey;
+            }
+            return Colors.black38;
+          }),
         ),
         onPressed: () {
-
           game.gamePlay(myController);
 
           setState(() {
             buttonText = game.activeButtonText;
             textInfo = game.infoText();
           });
-
         },
         child: Text(
           buttonText,
           style: globalTextStyle,
-        )
-    );
+        ));
   }
 
   ElevatedButton surrenderButton() {
     return ElevatedButton(
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                if (states.contains(MaterialState.disabled)) {
-                  return Colors.grey;
-                }
-                return Colors.black38;
-              }),
+              (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return Colors.grey;
+            }
+            return Colors.black38;
+          }),
         ),
         onPressed: () {
-
           game.gameStage = 3;
           game.gamePlay(myController);
 
@@ -158,12 +141,10 @@ class _BullsAndCowsScreenState extends State<BullsAndCowsScreen> {
             buttonText = game.activeButtonText;
             textInfo = game.infoText();
           });
-
         },
         child: Text(
           'Surrender',
           style: globalTextStyle,
-        )
-    );
+        ));
   }
 }
